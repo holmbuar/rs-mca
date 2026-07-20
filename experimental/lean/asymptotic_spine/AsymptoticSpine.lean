b4cb23f26f94fcd683797c1c4178791d8e8213f6
holmbuar <17097262+holmbuar@users.noreply.github.com>
2026-07-20 09:41:28 +0200

    Import C7 earlier-line extension

--- experimental/lean/asymptotic_spine/AsymptoticSpine.lean
+++ experimental/lean/asymptotic_spine/AsymptoticSpine.lean
@@ -8,6 +8,7 @@ import AsymptoticSpine.UniformClosedLedger
 import AsymptoticSpine.C7OwnerRegression
 import AsymptoticSpine.C7BasePoleProducer
 import AsymptoticSpine.C7BasePoleWitnessProducer
+import AsymptoticSpine.C7BasePoleLineExtension
 import AsymptoticSpine.BooleanFiber
 import AsymptoticSpine.NoHighEnergy
 import AsymptoticSpine.SigmaDiagonal
